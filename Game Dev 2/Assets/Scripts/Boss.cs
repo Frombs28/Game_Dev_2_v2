06d05862691a92010c90c53b6986be6e5f99698c
Steve <frombs@rpi.edu>
2019-04-17 03:07:04 -0400

    Give boss a health bar, and door

---
+++ Game Dev 2/Assets/Scripts/Boss.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Boss : MonoBehaviour
+{
+
+    public GameObject door;
+    public Transform spot;
+    int health = 80;
+    public Slider health_bar;
+
+    // Use this for initialization
+    void Start()
+    {
+        health_bar.maxValue = health;
+        health_bar.value = health;
+        Instantiate(door, spot);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collider)
+    {
+        if (collider.gameObject.tag == "Projectile")
+        {
+            if (collider.gameObject.layer == 9)
+            {
+                health -= collider.gameObject.GetComponent<Bullet>().GetDamage();
+                health_bar.value = health;
+                if(health <= 0)
+                {
+                    Destroy(door);
+                }
+            }
+        }
+    }
+}
